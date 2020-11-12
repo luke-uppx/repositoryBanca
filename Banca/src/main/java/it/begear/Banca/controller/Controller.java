1f06b1e53054fe6d210c5bc6bca258e967a7d177
luke-uppx <sorvillo.luca.1@gmail.com>
2020-11-12 17:04:49 +0100

    commit

---
+++ Banca/src/main/java/it/begear/Banca/controller/Controller.java
@@ -0,0 +1,9 @@
+package it.begear.Banca.controller;
+
+public class Controller {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Banca/src/main/java/it/begear/Banca/entity/Cliente.java
@@ -0,0 +1,9 @@
+package it.begear.Banca.entity;
+
+public class Cliente {
+
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
